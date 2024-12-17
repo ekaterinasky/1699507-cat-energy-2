64836dabcb43939fbb660909d4808e08677b9744
Katerina Sklyarova <esidor27@mail.ru>
2024-12-17 16:47:51 +0300

    Мобильное меню, часть, слайдера, google-карта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,15 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav');
+const navMain = document.querySelector('.main-nav__user');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav__user--nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__user--closed')) {
+    navMain.classList.remove('main-nav__user--closed');
+    navMain.classList.add('main-nav__user--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__user--closed');
+    navMain.classList.remove('main-nav__user--opened');
   }
 });
