ce37e8f747f8ad93ce02b4f6f423563a7d2725a9
Katerina Sklyarova <esidor27@mail.ru>
2025-01-05 14:34:35 +0300

    PixelPerfect index desktop

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,1 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav__user');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav__user--nojs');
-navMain.classList.remove('main-nav__user--opened');
-
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav__user--closed')) {
-    navMain.classList.remove('main-nav__user--closed');
-    navMain.classList.add('main-nav__user--opened');
-  } else {
-    navMain.classList.add('main-nav__user--closed');
-    navMain.classList.remove('main-nav__user--opened');
-  }
-});
